aea9ae9c29963bd57be463f9aafe61fe9f79d721
jonathonwingson <jonathonwingson@gmail.com>
2023-06-07 15:13:49 +0000

    backend changed to sqlite

--- db.js
+++ db.js
@@ -1,45 +1,20 @@
 // imports sequelize module
 const Sequelize = require("sequelize");
 
-// imports variable to enable the database url to be hidden
-require("dotenv").config();
-
 // connects database to server
-
-
-const db = new Sequelize(
-    'advisor-app-uwi', //database name
-    'advisor-app-uwi', //username
-    'phe3aeB5yopha7sh', //password
-    {
-        host: 'postgres_14',
-        port: 5432,
-        dialect: 'postgres',
-        pool: {
-            max: 3, 
-            min: 0,
-            idle: 10000,
-        }
-    });
-
-/*
-const db = new Sequelize(
-    "postgres://agdbblsa:sky9sH57Uf4QAM-eP71ooyYlaM9VLJHJ@ziggy.db.elephantsql.com:5432/agdbblsa", {
-    pool: {
-        max: 3,
-        min: 0,
-        idle: 10000,
-    },
+const db = new Sequelize({
+  dialect: "sqlite",
+  storage: "db-sqlite", // specify the path to your SQLite database file
+  pool: {
+    max: 3,
+    min: 0,
+    idle: 10000,
+  },
 });
-*/
 
-
-
-// tests database connection on server start up. To see if connection is OK.
-
+// tests database connection on server startup to see if the connection is OK.
 db.authenticate()
-    .then(() => console.log("Database Connected"))
-    .catch((err) => console.log("Error: " + err));
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log("Error: " + err));
 
 module.exports = db;
- 
