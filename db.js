8e2d7bb3d70a7f91228fb4f59c0f3ed1fcac433c
jWing <78874755+jonathonwingson@users.noreply.github.com>
2023-06-09 18:24:37 +0000

    db password change

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const db = new Sequelize({
   port: "5432",
   database: "myadvisor_database",
   username: "myadvisor_database_user",
-  password: process.env.DB_PASSWORD,
+  password: "YuKpP0lz6KhxNnsRLExqDQrl64bJj6OS",
   pool: {
     max: 3,
     min: 0,
