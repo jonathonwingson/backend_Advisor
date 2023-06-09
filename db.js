80bc8be3780ff20e32641ae32c2004deed633296
jWing <78874755+jonathonwingson@users.noreply.github.com>
2023-06-09 22:57:47 +0000

    db.js fix

--- db.js
+++ db.js
@@ -1,20 +1,33 @@
 // imports sequelize module
 const Sequelize = require("sequelize");
 
+const url = 'postgres://myadvisor_database_user:YuKpP0lz6KhxNnsRLExqDQrl64bJj6OS@dpg-ci1kmm0rddl1m6hknfbg-a.oregon-postgres.render.com/myadvisor_database';
+
+// Extracting database connection information from the URL
+const [, dialect, username, password, host, database] = url.match(/^(postgres):\/\/([^:]+):([^@]+)@([^/]+)\/(.+)$/);
+
 // connects database to server
-const db = new Sequelize({
-  dialect: "postgres",
-  host: "postgres://YuKpP0lz6KhxNnsRLExqDQrl64bJj6OS@dpg-ci1kmm0rddl1m6hknfbg-a.oregon-postgres.render.com/myadvisor_database",
-  port: "5432",
-  database: "myadvisor_database",
-  username: "myadvisor_database_user",
-  password: "YuKpP0lz6KhxNnsRLExqDQrl64bJj6OS",
-  pool: {
-    max: 3,
-    min: 0,
-    idle: 10000,
-  },
+const db = new Sequelize(database, username, password, {
+  host,
+  dialect,
+  dialectOptions: {
+    ssl: true,
+  }
 });
+
+// const db = new Sequelize({
+//   dialect: "postgres",
+//   host: "YuKpP0lz6KhxNnsRLExqDQrl64bJj6OS@dpg-ci1kmm0rddl1m6hknfbg-a.oregon-postgres.render.com/myadvisor_database",
+//   port: "5432",
+//   database: "myadvisor_database",
+//   username: "myadvisor_database_user",
+//   password: "YuKpP0lz6KhxNnsRLExqDQrl64bJj6OS",
+//   pool: {
+//     max: 3,
+//     min: 0,
+//     idle: 10000,
+//   },
+// });
 
 // tests database connection on server startup to see if the connection is OK.
 db.authenticate()
@@ -22,3 +35,29 @@ db.authenticate()
   .catch((err) => console.log("Error: " + err));
 
 module.exports = db;
+
+
+// const { Sequelize } = require('sequelize');
+
+// const url = 'postgres://myadvisor_database_user:YuKpP0lz6KhxNnsRLExqDQrl64bJj6OS@dpg-ci1kmm0rddl1m6hknfbg-a.oregon-postgres.render.com/myadvisor_database';
+
+// // Extracting database connection information from the URL
+// const [, dialect, username, password, host, database] = url.match(/^(postgres):\/\/([^:]+):([^@]+)@([^/]+)\/(.+)$/);
+
+// const sequelize = new Sequelize(database, username, password, {
+//   host,
+//   dialect,
+// });
+
+// // Test the database connection
+// (async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log('Connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   } finally {
+//     // Close the database connection when done
+//     await sequelize.close();
+//   }
+// })();
